75376d5d8783e73c74a32a01f9f34bbc03c1d5ec
Ashton Lima <ashton@4rden.com>
2025-09-12 13:25:31 -0700

    restart

--- script.js
+++
@@ -1,8 +0,0 @@
-const input = document.getElementById("number-input");
-
-input.addEventListener("input", () => {
-  let val = input.value;
-
-  if (val.length > 3) {
-    input.value = val.slice(0, -1);
-  }})
